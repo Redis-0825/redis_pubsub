fb0ee9c6fc338e3cb9fde2bcfe3012972540d931
mhasan8801 <hasanmuhammad197@gmail.com>
2025-08-20 18:35:27 +0700

    subscriber

--- redis_pubsub.js
+++ redis_pubsub.js
@@ -3,3 +3,7 @@
 //Jika tidak ada Consumer/Publisher, maka Redis tidak akan mengirimkan data apapun, dan secara otomatis datanya akan hilang
 //Oleh karena itu, jangan gunakan Redis PubSub sebagai media penyimpanan atau untuk antrian
 
+//Subscriber
+//Jika kita mengirim data ke Channel, tapi belum ada Subscriber nya, secara otomatis datanya akan hilang, oleh karena itu ada baiknya di awal kita menjalankan Subscriber nya terlebih dahulu
+subscribe chat notification //sesi 1 subscribe ke chatt & notification
+subscribe chat //sesi lainnya hanya subscribe ke chat
